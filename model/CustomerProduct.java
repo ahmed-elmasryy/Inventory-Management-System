bdb8ad42a89a7d50caa062edeab13e70bbfc4a39
ahmedwael <dodowael0@gmail.com>
2025-10-18 02:33:35 +0300

    update CustomerProduct

--- model/CustomerProduct.java
+++ model/CustomerProduct.java
@@ -1,3 +1,47 @@
-public class CustomerProduct{
 
+import java.time.LocalDate;
+
+public class CustomerProduct {
+
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        paid = false;
+    }
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public String lineRepresentation() {
+        return (customerSSN + "," + productID + "," + purchaseDate + "," + paid + ".");
+    }
+
+    public boolean isPaid() {
+        if(paid == true) {
+            return true;
+        }
+        return false;
+    }
+public String getSearchKey()
+{
+     return (customerSSN + "," + productID + "," + purchaseDate + ".");
+}
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
 }
