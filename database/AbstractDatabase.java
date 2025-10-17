39283f1a27baa4d562cef8fceed486e010e69b75
ahmedwael <dodowael0@gmail.com>
2025-10-17 04:12:10 +0300

    Abstract Database

--- database/AbstractDatabase.java
+++ database/AbstractDatabase.java
@@ -1,5 +1,23 @@
-public abstract class AbstractDatabase{
-int x=5;
-//fbdf
 
+
+import java.util.*;
+public abstract class AbstractDatabase<T> {
+
+    protected ArrayList<T> records = new ArrayList<>();
+    protected String filename;
+public AbstractDatabase(String filename)
+{
+this.filename=filename;
+}
+public abstract void readFromFile();
+public abstract T createRecordFrom(String line);
+public ArrayList<T> returnAllRecords()
+{
+    return records;
+}
+public abstract boolean contains(String key ) ;
+public abstract T getRecord(String key);
+public abstract void insertRecord(T record); 
+public abstract void deleteRecord(String key);
+public abstract void saveToFile();
 }
